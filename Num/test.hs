0716f450cadfc060ea332ebca2ed0cd12efe4554
John Goerzen <jgoerzen@complete.org>
2005-06-24 11:21:02 -0500

    Added Units class
    darcs-hash:20050624162102-c2a52-bd8a428f61c6536f107ed2bd28738676c02b35d8.gz

--- Num/test.hs
+++ Num/test.hs
@@ -47,3 +47,65 @@ instance Floating SymbolicManip where
     asinh a = UnaryArith "asinh" a
     acosh a = UnaryArith "acosh" a
     atanh a = UnaryArith "atanh" a
+
+data Units = Units Double SymbolicManip
+           deriving (Eq, Show)
+instance Num Units where
+    (Units xa ua) + (Units xb ub) 
+        | ua == ub = Units (xa + xb) ua
+        | otherwise = error "Mis-matched units in add"
+    (Units xa ua) - (Units xb ub) = (Units xa ua) + (Units (xb * (-1)) ub)
+    (Units xa ua) * (Units xb ub) = Units (xa * xb) (ua * ub)
+    negate (Units xa ua) = Units (negate xa) ua
+    abs (Units xa ua) = Units (abs xa) ua
+    signum (Units xa ua) = Units (signum xa) (Number 1)
+    fromInteger i = Units (fromInteger i) (Number 1)
+
+instance Fractional Units where
+    (Units xa ua) / (Units xb ub) = Units (xa / xb) (ua / ub)
+    recip a = 1 / a
+    fromRational r = Units (fromRational r) (Number 1)
+
+instance Floating Units where
+    pi = (Units pi (Number 1))
+    exp _ = error "exp not yet implemented in Units"
+    log _ = error "log not yet implemented in Units"
+    (Units xa ua) ** (Units xb ub) 
+        | ub == Number 1 = Units (xa ** xb) (ua ** Number xb)
+        | otherwise = error "units for RHS of ** not supported"
+    sqrt (Units xa ua) = Units (sqrt xa) (sqrt ua)
+    sin (Units xa ua) 
+        | ua == Symbol "rad" = Units (sin xa) (Number 1)
+        | ua == Symbol "deg" = Units (sin (deg2rad xa)) (Number 1)
+        | otherwise = error "Units for sin must be deg or rad"
+    cos (Units xa ua) 
+        | ua == Symbol "rad" = Units (cos xa) (Number 1)
+        | ua == Symbol "deg" = Units (cos (deg2rad xa)) (Number 1)
+        | otherwise = error "Units for cos must be deg or rad"
+    tan (Units xa ua)
+        | ua == Symbol "rad" = Units (tan xa) (Number 1)
+        | ua == Symbol "deg" = Units (tan (deg2rad xa)) (Number 1)
+        | otherwise = error "Units for tan must be deg or rad"
+    asin (Units xa ua) 
+        | ua == Number 1 = Units (rad2deg $ asin xa) (Symbol "deg")
+        | otherwise = error "Units for asin must be empty"
+    acos (Units xa ua)
+        | ua == Number 1 = Units (rad2deg $ acos xa) (Symbol "deg")
+        | otherwise = error "Units for acos must be empty"
+    atan (Units xa ua)
+        | ua == Number 1 = Units (rad2deg $ atan xa) (Symbol "deg")
+        | otherwise = error "Units for atan must be empty"
+    sinh = error "sinh not yet implemented in Units"
+    cosh = error "cosh not yet implemented in Units"
+    tanh = error "tanh not yet implemented in Units"
+    asinh = error "asinh not yet implemented in Units"
+    acosh = error "acosh not yet implemented in Units"
+    atanh = error "atanh not yet implemented in Units"
+
+units :: Double -> String -> Units
+units a b = Units a (Symbol b)
+                                                    
+    
+deg2rad x = 2 * pi * x / 360
+rad2deg x = 360 * x / (2 * pi)
+
